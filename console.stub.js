b3159857bee252f6292a2cd44eca529c1874145f
John Daly <jd@mdc.de>
2014-05-07 21:23:28 +0200

    init commit

---
+++ console.stub.js
@@ -0,0 +1,25 @@
+if (!window.console) {
+  console = {
+    assert:         function() {},
+    clear:          function() {},
+    count:          function() {},
+    debug:          function() {},
+    dir:            function() {},
+    dirxml:         function() {},
+    error:          function() {},
+    exception:      function() {},
+    group:          function() {},
+    groupCollapsed: function() {},
+    groupEnd:       function() {},
+    info:           function() {},
+    log:            function() {},
+    profile:        function() {},
+    profileEnd:     function() {},
+    table:          function() {},
+    time:           function() {},
+    timeEnd:        function() {},
+    timeStamp:      function() {},
+    trace:          function() {},
+    warn:           function() {}
+  };
+}
